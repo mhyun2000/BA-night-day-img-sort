6d77d99646c132546f291d2f2d8ecb3a6d08f61c
Minho Hyun <mhyun@ncsu.edu>
2023-06-27 02:12:52 -0400

    Added day folder/files creation and cleaned up methods and existing file cases

--- bg-seperator.py
+++ bg-seperator.py
@@ -1,8 +1,8 @@
 # Separates night and day images into separate folders
 # later scalable by accepting dir names and string instead of "_Night"
 
-# Iterate through the images and find the ones with "_Night" in the name
-# TODO: Remove the word "_Night" from the file name and add them to a list
+# side note: almost all files with the string "_Night" has a matching day file
+# but not all day files have a matching night file
 
 import os
 import shutil
@@ -13,29 +13,50 @@ nightDir = 'Separated/Night'
 dayDir = 'Separated/Day'
 keyword = '_Night'
 
+# list that holds the names of the files without the keyword
+dayList = []
 
-def main():
+def createNightDir():
     print("Starting...")
-    if not os.path.exists(nightDir):
+    if not os.path.isdir(nightDir):
         os.makedirs(nightDir)
-    if not os.path.exists(dayDir):
-        os.makedirs(dayDir)
+    
     for rootdir, _, files in os.walk(rootDir):
         existingFileCounter = 0
         for file in files:
             if keyword in file:
-                # copy file to new directory using shutil.copy function
-                try: 
-                    shutil.copy(os.path.join(rootdir, file), nightDir)
-                except shutil.SameFileError:
+                root_filename = os.path.basename(os.path.join(rootdir, file))
+                dest_filename = os.path.basename(os.path.join(nightDir, file))
+
+                # adding the filename to the dayList after removing the keyword
+                day_filename = root_filename.replace(keyword, "")
+                dayList.append(day_filename)
+                # print(day_filename)
+
+                # checking if the filename exists in the destination folder
+                if root_filename == dest_filename:
                     existingFileCounter += 1
-                    shutil.copy2(os.path.join(rootdir, file), nightDir)
+                else:
+                    shutil.copy(os.path.join(rootdir, file), nightDir)
         # print the number of files that already existed if any
         if existingFileCounter > 0:
             print(str(existingFileCounter) + " files with the same name have been overwritten." )
 
+def createDayDir():
+    if not os.path.isdir(dayDir):
+        os.makedirs(dayDir)
+    
+    for name in dayList:
+        fileName = os.path.join(rootDir, name)
+        # if fileName exists in rootDir
+        if os.path.exists(fileName):
+            shutil.copy(fileName, dayDir)
 
 
+def main():
+    createNightDir()
+    createDayDir()
+    
 main()
 
             
